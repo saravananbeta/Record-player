4a2570dcd623b4516a99617ac81774f9e985405f
saravananbeta <saravanan.beta@gmail.com>
2026-09-07 19:13:24 +0530

    Update script.js
    To display movie titles and song titles alternatively.

--- script.js
+++ script.js
@@ -139,6 +139,7 @@ fetch('data.json')
           if (metaElem) {
             metaElem.innerText = `${track.Singer} • ${track.Year} • ${trackNumber}/${totalTracks} ${shuffleIcon}`;
           }
+			eraser(videoId);
         }
       } catch (err) {
         console.error("Error inside fetchTrackTitle:", err);
@@ -414,3 +415,27 @@ function onPlayerStateChange(event) {
         document.getElementById('sleepMenu').classList.remove('is-open');
       }
     });    
+
+  function eraser(videoId) {
+ 
+    const Id = videoId;
+    const track = localPlaylistData[Id];
+    if (!track) return;
+     
+    // 1. Kill old 10s loop if any
+    if (toggleTimer !== null) {
+        clearInterval(toggleTimer);
+    }
+
+    // 2. Reset state and show Title immediately
+    showMovie = false;
+    document.getElementById('trackTitle').innerText = track.Title;
+
+    // 3. Start new loop - toggle every 10 sec
+    toggleTimer = setInterval(() => {
+        showMovie = !showMovie;
+        document.getElementById('trackTitle').innerText = showMovie ? track.Movie : track.Title;
+        //document.getElementById('trackTitle').innerText = showMovie ? 'Movie' : 'Song';
+    }, 5000);
+} 
+
